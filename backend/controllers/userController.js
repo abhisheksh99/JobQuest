dc634f7dcb6758f7f7546600f771ced58f909c98
abhisheksh99 <abhishekleo1234@gmail.com>
2025-01-21 22:29:41 -0500

    Multer added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,6 +6,8 @@ import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   try {
     const { fullName, email, phoneNumber, password, role } = req.body;
+    console.log(fullName, email, phoneNumber, password, role);
+    
     const file = req.file;
 
     //cloudinary
